cf01d5104b0160a9700312e70478e0e663af8fec
alex fierascu <alexandru.fierascu@cegeka.com>
2021-06-22 20:03:11 +0200

    add multiple tests

---
+++ selenium/src/test/java/SimpleDedemanSeleniumTest.java
@@ -0,0 +1,62 @@
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class SimpleDedemanSeleniumTest {
+
+
+  @Test
+  public void loginIntoDedemanMainApplication() {
+    //set the property for the chrome.driver to know where the driver is located
+    //depending on the OS and browser version you are using we set this property
+    //read more - https://www.selenium.dev/documentation/en/webdriver/driver_requirements/
+    System.setProperty("webdriver.chrome.driver",
+        "C:\\!dev_projects\\sda\\grupa16\\selenium\\src\\test\\resources\\webdriver\\windows\\chromedriver.exe");
+    //initialize the driver as ChromeDriver
+    WebDriver driver = new ChromeDriver();
+    //maximize the browser window
+    driver.manage().window().maximize();
+    //navigate to URL
+    driver.get("https://www.dedeman.ro/");
+
+    WebElement buttonContulMeu = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/span"));
+
+    //Creating object of an Actions class
+        Actions action = new Actions(driver);
+
+    //Performing the mouse hover action on the target element.
+        action.moveToElement(buttonContulMeu).perform();
+
+    WebElement buttonAutentificare = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[1]/div/div[2]/div[2]/div[2]/ul/li[2]/div/div[2]/a[1]"));
+    buttonAutentificare.click();
+
+
+    WebElement inputJudetElement = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[3]/div/div/div[1]/div/input"));
+    inputJudetElement.click();
+    inputJudetElement.sendKeys("Iasi");
+    inputJudetElement.sendKeys(Keys.RETURN);
+
+    WebElement inputLocalitateElement = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[3]/div/div/div[2]/div/input"));
+    inputLocalitateElement.click();
+    inputLocalitateElement.sendKeys("Albesti");
+    inputJudetElement.sendKeys(Keys.RETURN);
+
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    WebElement inputClientEmailAddress = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[3]/div/div[1]/form/div[1]/input"));
+    inputClientEmailAddress.click();
+    inputClientEmailAddress.sendKeys("fier4jat@yahoo.com");
+
+    WebElement inputClientPassword = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[3]/div/div[1]/form/div[2]/input"));
+    inputClientPassword.click();
+    inputClientPassword.sendKeys("Prastiex2021!@");
+
+    WebElement buttonAutentificareClient = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div[2]/div[3]/div/div[1]/form/button/div"));
+    buttonAutentificareClient.click();
+
+  }
+}
---
+++ selenium/src/test/java/SimpleEmagSeleniumTest.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleEmagSeleniumTest {
+
+
+  @Test
+  public void openEmagPage() {
+
+    //set the property for the chrome.driver to know where the driver is located
+    //depending on the OS and browser version you are using we set this property
+    //read more - https://www.selenium.dev/documentation/en/webdriver/driver_requirements/
+    System.setProperty("webdriver.chrome.driver",
+        "C:\\!dev_projects\\sda\\grupa16\\selenium\\src\\test\\resources\\webdriver\\windows\\chromedriver.exe");
+    //initialize the driver as ChromeDriver
+    WebDriver driver = new ChromeDriver();
+    //maximize the browser window
+    driver.manage().window().maximize();
+    //navigate to URL
+    driver.get("https://www.emag.ro/");
+
+    WebElement buttonEmagGenius = driver.findElement(By.xpath("//*[@id=\"auxiliary\"]/div/div/ul[2]/li[1]"));
+    buttonEmagGenius.click();
+
+    WebElement buttonEmagGeniusIncearcaGratuit = driver.findElement(By.xpath("//*[@id=\"main-container\"]/div[4]/form/div[1]/label/span[2]/a"));
+    buttonEmagGeniusIncearcaGratuit.click();
+
+    WebElement buttonEmagContinuaLogin = driver.findElement(By.id("user_login_continue"));
+
+    WebElement inputEmailField = driver.findElement(By.id("user_login_email"));
+    inputEmailField.click();
+    inputEmailField.sendKeys("fier4jat@yahoo.com");
+    buttonEmagContinuaLogin.click();
+
+    WebElement inputPasswordField = driver.findElement(By.id("user_login_password"));
+    inputPasswordField.click();
+    inputPasswordField.sendKeys("hamleys,28.");
+    buttonEmagContinuaLogin.click();
+
+    //close the driver instance
+    driver.close();
+
+  }
+
+}
